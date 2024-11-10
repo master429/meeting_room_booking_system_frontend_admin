8a2b6d47ccaa2fc87a21afd4076763417b6d55bc
liuqingpo <1432003391@qq.com>
2024-11-10 18:38:49 +0800

    docker

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -8,7 +8,7 @@ import { SearchBooking } from "../pages/BookingManage/BookingManage";
 import dayjs from "dayjs";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3005/",
+  baseURL: "http://101.200.85.79/api",
   timeout: 3000,
 });
 
